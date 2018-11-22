10e7302857c51e12221993dc9026335c22bd670d
Aliaksandr Paukalas <alexander.povkolas@gmail.com>
2018-11-22 18:16:52 +0300

    Ref

--- main.cpp
+++ main.cpp
@@ -15,23 +15,25 @@ string get_process_data(int rank) {
     return "Hey. I'm process # " + to_string(rank) + " " + random;
 }
 
+#define ROOT 0
+#define COMMON_TAG 1
 int main(int argc, char **argv) {
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
 
     int world_size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     vector<string> result;
-    if (rank == 0) {
+    if (rank == ROOT) {
         for (int i = 1; i < world_size; i++) {
             MPI_Status status;
-            MPI_Probe(MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+            MPI_Probe(MPI_ANY_SOURCE, COMMON_TAG, MPI_COMM_WORLD, &status);
             int lenght;
             MPI_Get_count(&status, MPI_CHAR, &lenght);
             string buffer(lenght, ' ');
 
-            MPI_Recv((char *) buffer.c_str(), lenght, MPI_CHAR, status.MPI_SOURCE, 1, MPI_COMM_WORLD,
+            MPI_Recv((char *) buffer.c_str(), lenght, MPI_CHAR, status.MPI_SOURCE, COMMON_TAG, MPI_COMM_WORLD,
                      MPI_STATUS_IGNORE);
             result.push_back(buffer);
         }
@@ -43,7 +45,7 @@ int main(int argc, char **argv) {
 
     } else {
         string s = get_process_data(rank);
-        MPI_Send(s.c_str(), s.size(), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+        MPI_Send(s.c_str(), s.size(), MPI_CHAR, ROOT, COMMON_TAG, MPI_COMM_WORLD);
     }
 
 
